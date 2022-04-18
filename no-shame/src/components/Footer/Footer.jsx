f8ff0a342bd3490e64e64c7495a45c9c8e486726
Fernanda9421 <fer.candrade21@gmail.com>
2022-04-18 13:06:24 -0300

    feat: footer

---
+++ no-shame/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Container, Grid, Box, Link } from '@mui/material';
+import './footer.css';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <Box px={{xs: 3, sm: 10}} py={{xs: 5, sm: 6}} bgcolor='text.secondary' color='white'>
+        <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1} sx={{ fontWeight: 'bold' }}>SOBRE</Box>
+              <Box sx={{ lineHeight: 2.5, fontSize: 13 }}>
+                <Link href='/' underline='none' color='inherit'>NOSSA HISTÓRIA</Link>
+              </Box>
+              <Box sx={{ fontSize: 13 }}>
+                <Link href='/' underline='none' color='inherit'>LOCALIZAÇÃO</Link>
+              </Box>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1} sx={{ fontWeight: 'bold' }}>SUPORTE</Box>
+              <Box sx={{ lineHeight: 2.5, fontSize: 13 }}>
+                <Link href='/' underline='none' color='inherit'>CONTATO</Link>
+              </Box>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Box sx={{ fontWeight: 'bold', fontSize: 35 }} textAlign='center'>NO</Box>
+              <Box sx={{ fontWeight: 'bold', fontSize: 35 }} textAlign='center'>SHAME</Box>
+            </Grid>
+          </Grid>
+          <Box textAlign='center' pt={{xs: 5, sm:5}} pb={{xs:5, sm:0}}>
+            No Shame &copy; {new Date().getFullYear()}
+          </Box>
+        </Container>
+      </Box>
+    </footer>
+  )
+}
