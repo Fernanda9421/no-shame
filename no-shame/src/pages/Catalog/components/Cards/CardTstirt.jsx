cc0af2260d4a0528dadb75b45c8728e3f2659689
Fernanda9421 <fer.candrade21@gmail.com>
2022-04-20 15:20:37 -0300

    feat: cria componente e estilização de cards das t-shirts

---
+++ no-shame/src/pages/Catalog/components/Cards/CardTstirt.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import './cardTshirt.css';
+
+export default function ActionAreaCard() {
+  return (
+    <div className='div'>
+      <Card className='card-area' sx={{ maxWidth: 230 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="230"
+            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
+            alt="green iguana"
+          />
+          <CardContent className='card-content'>
+            <Typography className='typography' gutterBottom component="div">
+              Stay Positive - Black
+            </Typography>
+            <Typography className='typography' variant="body2">
+              Preço: 39,90
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card className='card-area' sx={{ maxWidth: 230 }}>
+        <CardActionArea className='card-area'>
+          <CardMedia
+            component="img"
+            height="230"
+            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
+            alt="green iguana"
+          />
+          <CardContent className='card-content'>
+            <Typography gutterBottom component="div">
+              Stay Positive - Black
+            </Typography>
+            <Typography variant="body2">
+              Preço: 39,90
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card className='card-area' sx={{ maxWidth: 230 }}>
+        <CardActionArea className='card-area'>
+          <CardMedia
+            component="img"
+            height="230"
+            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
+            alt="green iguana"
+          />
+          <CardContent className='card-content'>
+            <Typography gutterBottom component="div">
+              Stay Positive - Black
+            </Typography>
+            <Typography variant="body2">
+              Preço: 39,90
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card className='card-area' sx={{ maxWidth: 230 }}>
+        <CardActionArea className='card-area'>
+          <CardMedia
+            component="img"
+            height="230"
+            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
+            alt="green iguana"
+          />
+          <CardContent className='card-content'>
+            <Typography gutterBottom component="div">
+              Stay Positive - Black
+            </Typography>
+            <Typography variant="body2">
+              Preço: 39,90
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card className='card-area' sx={{ maxWidth: 230 }}>
+        <CardActionArea className='card-area'>
+          <CardMedia
+            component="img"
+            height="230"
+            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
+            alt="green iguana"
+          />
+          <CardContent className='card-content'>
+            <Typography gutterBottom component="div">
+              Stay Positive - Black
+            </Typography>
+            <Typography variant="body2">
+              Preço: 39,90
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card className='card-area' sx={{ maxWidth: 230 }}>
+        <CardActionArea className='card-area'>
+          <CardMedia
+            component="img"
+            height="230"
+            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
+            alt="green iguana"
+          />
+          <CardContent className='card-content'>
+            <Typography gutterBottom component="div">
+              Stay Positive - Black
+            </Typography>
+            <Typography variant="body2">
+              Preço: 39,90
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card className='card-area' sx={{ maxWidth: 230 }}>
+        <CardActionArea className='card-area'>
+          <CardMedia
+            component="img"
+            height="230"
+            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
+            alt="green iguana"
+          />
+          <CardContent className='card-content'>
+            <Typography gutterBottom component="div">
+              Stay Positive - Black
+            </Typography>
+            <Typography variant="body2">
+              Preço: 39,90
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
+}
