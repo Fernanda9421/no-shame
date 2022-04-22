c19dee117e04e9f738cc28b08a7d90fba2ed0510
Fernanda9421 <fer.candrade21@gmail.com>
2022-04-22 10:44:25 -0300

    Adiciona Sizes no componente Catalog

--- no-shame/src/pages/Catalog/Catalog.jsx
+++ no-shame/src/pages/Catalog/Catalog.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Cards from './components/Cards/CardTstirt';
 import ImageCatalog from './components/ImageCatalog/ImageCatalog';
+import Sizes from './components/Sizes/Sizes';
 
 export default function Catalog() {
   return (
     <>
       <ImageCatalog />
+      <Sizes />
       <Cards />
     </>
   );
