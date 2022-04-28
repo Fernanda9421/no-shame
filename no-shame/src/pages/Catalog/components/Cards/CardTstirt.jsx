a39875fa0b309981701dcd707e2109ac09735394
Fernanda9421 <fer.candrade21@gmail.com>
2022-04-28 07:16:59 -0300

    fix: deixa cards individuais de forma dinâmica

--- no-shame/src/pages/Catalog/components/Cards/CardTstirt.jsx
+++ no-shame/src/pages/Catalog/components/Cards/CardTstirt.jsx
@@ -3,138 +3,35 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, Box } from '@mui/material';
+import tShirtsGG from '../../../../data/size-GG';
 import './cardTshirt.css';
 
 export default function ActionAreaCard() {
   return (
-    <div className='div'>
-      <Card className='card-area' sx={{ maxWidth: 230 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="230"
-            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
-            alt="green iguana"
-          />
-          <CardContent className='card-content'>
-            <Typography className='typography' gutterBottom component="div">
-              Stay Positive - Black
-            </Typography>
-            <Typography className='typography' variant="body2">
-              Preço: 39,90
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className='card-area' sx={{ maxWidth: 230 }}>
-        <CardActionArea className='card-area'>
-          <CardMedia
-            component="img"
-            height="230"
-            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
-            alt="green iguana"
-          />
-          <CardContent className='card-content'>
-            <Typography gutterBottom component="div">
-              Stay Positive - Black
-            </Typography>
-            <Typography variant="body2">
-              Preço: 39,90
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className='card-area' sx={{ maxWidth: 230 }}>
-        <CardActionArea className='card-area'>
-          <CardMedia
-            component="img"
-            height="230"
-            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
-            alt="green iguana"
-          />
-          <CardContent className='card-content'>
-            <Typography gutterBottom component="div">
-              Stay Positive - Black
-            </Typography>
-            <Typography variant="body2">
-              Preço: 39,90
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className='card-area' sx={{ maxWidth: 230 }}>
-        <CardActionArea className='card-area'>
-          <CardMedia
-            component="img"
-            height="230"
-            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
-            alt="green iguana"
-          />
-          <CardContent className='card-content'>
-            <Typography gutterBottom component="div">
-              Stay Positive - Black
-            </Typography>
-            <Typography variant="body2">
-              Preço: 39,90
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className='card-area' sx={{ maxWidth: 230 }}>
-        <CardActionArea className='card-area'>
-          <CardMedia
-            component="img"
-            height="230"
-            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
-            alt="green iguana"
-          />
-          <CardContent className='card-content'>
-            <Typography gutterBottom component="div">
-              Stay Positive - Black
-            </Typography>
-            <Typography variant="body2">
-              Preço: 39,90
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className='card-area' sx={{ maxWidth: 230 }}>
-        <CardActionArea className='card-area'>
-          <CardMedia
-            component="img"
-            height="230"
-            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
-            alt="green iguana"
-          />
-          <CardContent className='card-content'>
-            <Typography gutterBottom component="div">
-              Stay Positive - Black
-            </Typography>
-            <Typography variant="body2">
-              Preço: 39,90
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className='card-area' sx={{ maxWidth: 230 }}>
-        <CardActionArea className='card-area'>
-          <CardMedia
-            component="img"
-            height="230"
-            image="https://d2r9epyceweg5n.cloudfront.net/stores/001/634/073/products/231-20248af5fb5581889216245890216789-320-0.png"
-            alt="green iguana"
-          />
-          <CardContent className='card-content'>
-            <Typography gutterBottom component="div">
-              Stay Positive - Black
-            </Typography>
-            <Typography variant="body2">
-              Preço: 39,90
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      {
+        tShirtsGG.map((item) => (
+          <Card className='card-area' sx={{ width: 230 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="230"
+                image={item.image}
+                alt={item.name}
+              />
+              <CardContent className='card-content'>
+                <Typography className='typography' gutterBottom component="div">
+                  {item.name}
+                </Typography>
+                <Typography className='typography' variant="body2">
+                  Preço: {item.price}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))
+      }
+    </Box>
   );
 }
